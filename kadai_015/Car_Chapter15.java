c3969790737a02bd3ef3706a99d1772623e7e384
sato-at <fukurou0art@gmail.ne.jp>
2024-04-14 20:15:18 +0900

    課題提出

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,34 @@
+package kadai_015;
+
+// 車クラス
+public class Car_Chapter15 {
+	// フィールド（内部データ）
+	private int gear;
+	private int speed;
+
+	// コンストラクタ（初期化処理）
+	public Car_Chapter15() {
+		gear =1;
+		speed = 10;
+	}
+
+	// ギアの値により速度を変える	
+	public void gearChange(int afterGear) {
+		this.gear = afterGear;
+		System.out.println("ギアを1から" + gear + "に切り替えました");
+
+		switch(this.gear) {
+		case 1 -> this.speed = 10;
+		case 2 -> this.speed = 20;
+		case 3 -> this.speed = 30;
+		case 4 -> this.speed = 40;
+		case 5 -> this.speed = 50;
+		default -> this.speed = 10;
+		}
+	}
+
+	// ギアチェンジ後の速度を表示する	
+	public void run() {
+		System.out.println("速度は時速" + speed + "kmです");
+	}
+}
