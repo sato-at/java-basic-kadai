4ce3f91c0d254d498d70b818252bfb501f7809d3
sato-at <fukurou0art@gmail.ne.jp>
2024-04-21 12:14:35 +0900

    課題提出

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,14 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+
+		Dictionary_Chapter21 search = new Dictionary_Chapter21();
+		search.dictionary("apple");
+		search.dictionary("banana");
+		search.dictionary("grape");
+		search.dictionary("orange");
+	}
+
+}
---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,32 @@
+package kadai_021;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+
+	public void dictionary(String word){
+
+		HashMap<String, String>  dictionaryList = new HashMap<String, String>();
+
+		dictionaryList.put("apple","りんご");
+		dictionaryList.put("peach","桃");
+		dictionaryList.put("banana","バナナ");
+		dictionaryList.put("lemon","レモン");
+		dictionaryList.put("pear","梨");
+		dictionaryList.put("kiwi","キウィ");
+		dictionaryList.put("strawberry","いちご");
+		dictionaryList.put("grape","ぶどう");
+		dictionaryList.put("muscat","マスカット");
+		dictionaryList.put("cherry","さくらんぼ");
+
+		String meaning = dictionaryList.get(word);
+
+		if(meaning != null) {
+			System.out.println(word + "の意味は" + meaning);
+		}else {
+			System.out.println(word + "は辞書に存在しません");
+		}
+
+
+	}
+}
